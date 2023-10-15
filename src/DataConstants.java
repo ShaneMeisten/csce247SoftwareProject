5d303b9f6ba9a7ae7e1e5f12a9f18ea33491e5b8
Aidan Godwin <aidancg@email.sc.edu>
2023-10-15 14:37:20 -0400

    Data Constants

---
+++ src/DataConstants.java
@@ -0,0 +1,62 @@
+public class DataConstants {
+  protected static final String USER_FILE_NAME = "Json/user.json";
+  protected static final String USER_NAME = "name";
+  protected static final String USER_ID = "id";
+  protected static final String USER_ROLE = "role";
+  protected static final String USER_PERMS = "adminPerms";
+  protected static final String USER_TEAM = "team";
+  protected static final String USER_USERNAME = "userName";
+  protected static final String USER_PASSWORD = "password";
+  protected static final String USER_PHONE = "phone";
+  protected static final String USER_POINT = "point";
+  protected static final String USER_EMAIL = "email";
+  protected static final String USER_CURRENT_PROJECTS = "currentProjects";
+  protected static final String USER_INVITED_PROJECTS = "invitedProjects";
+
+  protected static final String PROJECT_FILE_NAME = "Json/project.json";
+  protected static final String PROJECT_ID = "projectId";
+  protected static final String PROJECT_NAME = "name";
+  protected static final String PROJECT_TYPE = "type";
+  protected static final String PROJECT_LAYOUT = "layout";
+  protected static final String PROJECT_LEADERBOARD = "leaderboard";
+  protected static final String PROJECT_COLUMN = "column";
+  protected static final String PROJECT_HISTORY = "history";
+
+  protected static final String COLUMN_ID = "id";
+  protected static final String COLUMN_TITLE = "title";
+  protected static final String COLUMN_STATUS = "status";
+  protected static final String COLUMN_COMPLETION_TIME = "completionTime";
+  protected static final String COLUMN_CREATED_TIME = "createdTime";
+  protected static final String COLUMN_AUTHOR = "author";
+  protected static final String COLUMN_TASKS = "tasks";
+
+  protected static final String TASK_ID = "id";
+  protected static final String TASK_TITLE = "title";
+  protected static final String TASK_DESCRIPTION = "description";
+  protected static final String TASK_DUE_DATE = "dueDate";
+  protected static final String TASK_WEIGHT = "weight";
+  protected static final String TASK_COMMENT_THREAD = "commentThread";
+  protected static final String TASK_STATUS = "status";
+  protected static final String TASK_COMPLETION_TIME = "completionTime";
+  protected static final String TASK_ASSIGNED_USER = "assignedUser";
+  protected static final String TASK_AUTHOR = "author";
+  protected static final String TASK_TODO_LIST = "toDoList";
+
+  protected static final String COMMENT_THREAD_NAME = "name";
+  protected static final String COMMENT_THREAD_DESCRIPTION = "description";
+  protected static final String COMMENT_THREAD_AUTHOR = "author";
+  protected static final String COMMENT_THREAD_DATE = "date";
+  protected static final String COMMENT_THREAD_REPLY = "reply";
+  
+  protected static final String TODOLIST_ID = "id";
+
+  protected static final String REPLY_NAME = "name";
+  protected static final String REPLY_DESCRIPTION = "description";
+  protected static final String REPLY_AUTHOR = "author";
+  protected static final String REPLY_DATE = "date";
+
+  protected static final String HISTORY_ID = "id";
+  protected static final String HISTORY_TIMESTAMP = "timeStamp";
+  protected static final String HISTORY_USER = "user";
+  protected static final String HISTORY_CHANGE_LOG = "changelog";
+}
