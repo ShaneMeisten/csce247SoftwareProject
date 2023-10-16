d63db2e175b70a916683de456d27f47e882f6068
Rafael Prignano <prignano@email.sc.edu>
2023-10-15 21:09:52 -0400

    Finished #33

---
+++ src/ProjectCatalog.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class ProjectCatalog {
+    private ProjectCatalog projectCatalog;
+    public static ArrayList<Project> projects;
+
+    private ProjectCatalog() {
+
+    }
+
+    public projectList getInstance(){
+
+    }
+
+    public static  addProject(String ProjectName, String ProjectType){
+        return; // placeholder return type.
+    }
+
+    public static removeProject(UUID ProjectUUID){
+        return; //placeholder return type.
+    }
+
+    public static Project getProject(UUID ProjectUUID){
+        return null;
+
+    }
+
+
+
+
+
+}
