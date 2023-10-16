c14a2852bf76a74637510d048d0eed157beccf34
Rafael Prignano <prignano@email.sc.edu>
2023-10-15 20:32:26 -0400

    Project.java edited

--- src/Project.java
+++ src/Project.java
@@ -1,3 +1,78 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
+
+
+/**
+ * @author Rafael Prignano
+ */
+
+
 public class Project {
-    
+    private UUID id;
+    private String name;
+    private String type;
+    private Layout layout;
+    private ArrayList<Task> ongoingTasks;
+    private ArrayList<Task> completedTasks;
+    private ArrayList<User> leaderboard;
+    private ArrayList<Column> ColumnList;
+    private ArrayList<Task> TaskList;
+
+    public Project(String name , String type){
+
+    }
+
+    public boolean addTask(Task task){
+        return true; //placeholder
+
+    }
+
+    public boolean removeTask(Task task){
+        return true; //placeholder
+
+    }
+
+    public boolean addColumn(Column Column){
+        return true; //placeholder
+
+    }
+
+    public boolean addTaskToColumn(Task task, Column column){
+        return true; //placeholder
+
+    }
+
+    public boolean removeTaskToColumn(Task task, Column column){
+        return true; //placeholder
+
+    }
+
+    public ArrayList<column> viewColumn(String name){
+
+    }
+
+    public ArrayList<Task> viewColumnTask(String column , String task){
+
+    }
+
+    public Task viewUnassignedTask(String task){
+
+    }
+
+    public void setProjectStatus(Task task , boolean status){
+
+    }
+
+    public boolean addToLeaderboard(User user){
+        
+    }
+
+
+
+
+
+
+
+
 }
