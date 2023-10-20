6f1e9c22b7295fadf43042a157bc7a3128818c8d
mobbi <nevocamo@gmail.com>
2023-10-19 23:12:46 -0400

    fixed #10

--- src/History.java
+++ src/History.java
@@ -4,29 +4,48 @@ import java.util.*;
  */
 
 public class History {
-    private static History history;
-    private static ArrayList<Update> historyList = new ArrayList<Update>();
+    private  ArrayList<Update> historyList = new ArrayList<Update>();
 
     public History(){}
 
-    private static History getInstance(){
-        if (history == null) history = new History();
-        return history;
+    public void loadHistory( ArrayList<Update> historyList){
+        for (int i = 0; i < historyList.size(); i++){
+            this.historyList.add(historyList.get(i));
+        }
+        sortHistory();
     }
 
-    public static ArrayList<Update> getHistory(UUID userID){
+    public  ArrayList<Update> getHistory(UUID userID){
         return historyList;
     }
     
-    public static ArrayList<Update> accessHistoryChangedByUUID(UUID ChangedID){
-        return null;
+    //May change to merge sort in the future
+    private void sortHistory(){
+        if(historyList.size() == 0) return;
+        //Implement selection sort
+        //Resourced from https://www.geeksforgeeks.org/selection-sort/
+        for(int i = 0; i < historyList.size(); i++){
+            int hold = i;
+            for(int j = i + 1; j< historyList.size(); j++){
+                if(historyList.get(j).getDate().before(historyList.get(hold).getDate())) 
+                    hold = j;
+            }
+            Update holdUpdate = historyList.get(hold);
+            historyList.set(hold, historyList.get(i));
+            historyList.set(i, holdUpdate);
+        }
+    }
+    public  ArrayList<Update> accessHistoryChangedByUUID(UUID ChangedID){
+        ArrayList<Update> userUpdate = new ArrayList<Update>();
+        for (int i = 0; i < historyList.size(); i++){
+            if(historyList.get(i).getUserUUID().compareTo(ChangedID) == 0){
+                userUpdate.add(historyList.get(i));
+            }
+        }
+        return userUpdate;
     }
 
-    public static ArrayList<Update> accessHistoryUserByUUID(UUID ChangedID){
-        return null;
-    }
-
-    public static void add(Update update){
+    public void add(Update update){
         historyList.add(update);
     }
 }
