df273d24bba0e7c5cd5310130ad7b51bd9d75736
mobbi <nevocamo@gmail.com>
2023-10-20 23:13:18 -0400

    fixed #12

--- src/Comment.java
+++ src/Comment.java
@@ -12,6 +12,15 @@ public class Comment {
     private Date date;
     private ArrayList<Comment> replies;
 
+    public Comment(UUID id, String name, String description, UUID author, Date date, ArrayList<Comment> replies){
+        this.id = id;
+        this.author = author;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.replies = replies;
+    }
+
     public Comment(UUID author, String name, String description){
         this.id = UUID.randomUUID();
         this.author = author;
@@ -21,8 +30,30 @@ public class Comment {
         this.replies = new ArrayList<Comment>();
     }
 
+    public UUID getAuthorUUID(){
+        return author;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+
+    public String getName(){
+        return name;
+    }
+
     public void addReply(Comment comment){
         replies.add(comment);
+    }
+
+    public boolean removeComment(UUID author, Comment comment){
+        if(author.compareTo(this.author) != 0) return false;
+        replies.remove(comment);
+        return true;
     }
 
     public ArrayList<Comment> getReplies(){
