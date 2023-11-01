3b262637e470a39e708bad85d4294b5de89ccafb
CameronReyes <careyeumon2@gmail.com>
2023-11-01 17:01:40 -0400

    fixed #55

--- src/Update.java
+++ src/Update.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 /*
+ * Stores the content of changes for History
  * @author  Cameron Reyes
  */
 
@@ -10,6 +11,14 @@ public class Update {
     private UUID changedID;
     private String changeLog;
 
+    /*
+     * Normal constructor for Update
+     * 
+     * @param userUUID      The User who created the time stamp
+     * @param changedID     The project/task/column that was changed
+     * @param changeLog     The desciprtive change of what happend
+     */
+
     public Update(UUID userUUID, UUID changedID, String changeLog){
         this.timeStamp = new Date();
         this.userUUID = userUUID;
@@ -17,7 +26,15 @@ public class Update {
         this.changeLog = changeLog;
     }
 
-    //historyId, timeStamp, historyUser, changelog
+    /*
+     * Dataloader constructor for Update
+     * 
+     * @param changedID     The project/task/column that was changed
+     * @param timestamp     The date when this update was constructed
+     * @param userUUID      The User who created the time stamp
+     * @param changeLog     The desciprtive change of what happend
+     */
+
     public Update(UUID changedID, Date timeStamp, UUID userUUID, String changeLog){
         this.changedID = changedID;
         this.timeStamp = timeStamp;
@@ -25,21 +42,52 @@ public class Update {
         this.changeLog = changeLog;
     }
 
+    /*
+     * Returns the Date the update was made
+     * 
+     * @return timestamp    The date the update was made
+     */
+
     public Date getDate(){
         return timeStamp;
     }
+
+    /*
+     * Returns the user who made the Update
+     * 
+     * @return userUUID    the user who made the Update
+     */
 
     public UUID getUserUUID(){
         return userUUID;
     }
 
+    /*
+     * Returns the Project/Column/task where the update happened
+     * 
+     * @return changedID   the Project/Column/task where the update happened
+     */
+
     public UUID getChangedID(){
         return changedID;
     }
 
+    /*
+     * Returns the change in the update 
+     * 
+     * @return changeLog   the change in the update 
+     */
+    
+
     public String getChangedLog(){
         return changeLog;
     }
+
+    /*
+     * Printing the timestamp, userUUID, changedUUID and changeLog
+     * 
+     * @return toString     Prinable method of the update
+     */
 
     public String toString(){
         return "TIME: [" + timeStamp.toString() + "]  USER: [" + userUUID.toString() + "] Change: " + changeLog + 
